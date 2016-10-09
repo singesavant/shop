66a0be1ac053ad8d063bf04d8d58d853980d7b2b
Guillaume Libersat <glibersat@sigill.org>
2016-10-09 19:26:38 +0200

    fix user

--- apps/order/processing.py
+++ apps/order/processing.py
@@ -1,4 +1,5 @@
 from django.http import HttpRequest
+from django.contrib.auth.models import AnonymousUser
 
 from oscar.apps.order import processing
 from oscar.apps.checkout import mixins
@@ -9,7 +10,7 @@ class EventHandler(processing.EventHandler, mixins.OrderPlacementMixin):
         super(EventHandler, self).handle_order_status_change(order, new_status, note_msg)
 
         self.request = HttpRequest()
-        self.request.user = order.user
+        self.request.user = order.user or AnonymouseUser()
 
 
         self.send_confirmation_message(order, "ORDER_STATUS_CHANGE_{0}".format(new_status))
